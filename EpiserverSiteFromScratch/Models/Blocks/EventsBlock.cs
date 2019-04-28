4b0b64be6feaf462bae8a1375a3f524e0a9f2a4e
Fredrik Widén <fwiden@gmail.com>
2019-04-28 23:27:22 +0200

    fff

---
+++ EpiserverSiteFromScratch/Models/Blocks/EventsBlock.cs
@@ -0,0 +1,47 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Web;
+
+namespace EpiserverSiteFromScratch.Models.Blocks
+{
+    [ContentType(DisplayName = "EventsBlock", GUID = "06bdef31-65ba-4aaa-ba75-dbe97c3eab94", Description = "2 bilder")]
+    public class EventsBlock : BlockData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "header",
+            Description = "Name field's description",
+            GroupName = SystemTabNames.Content,
+            Order = 100)]
+        public virtual string Header { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "text under header",
+            Description = "Name field's description",
+            GroupName = SystemTabNames.Content,
+            Order = 200)]
+        public virtual string Text { get; set; }
+
+
+        [CultureSpecific]
+        [Display(
+            Name = "image",
+            Description = "Name field's description",
+            GroupName = SystemTabNames.Content,
+            Order = 300)]
+        [UIHint(UIHint.Image)]
+        public virtual ContentReference Image { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "image2",
+            Description = "Name field's description",
+            GroupName = SystemTabNames.Content,
+            Order = 300)]
+        [UIHint(UIHint.Image)]
+        public virtual ContentReference Image2 { get; set; }
+    }
+}
---
+++ EpiserverSiteFromScratch/Scripts/main.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () {
+    $(".click-header").click(function () {
+        console.log('Klick!');
+    });
+});
